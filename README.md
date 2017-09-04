2fbf92deafd6f95111969d5bdd9bd1a6abd118f0
sunorry <sunorry@gmail.com>
2017-09-04 23:22:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# vue-tpl
+
+for [mnz-scaffold](https://github.com/sunorry/mnz_scaffold) `mnz init` init the project template
+
---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'exports': [
+        './src/main.js',
+        './src/css/base.scss'
+    ],
+    'alias': {
+        '@': 'src'
+    },
+    sourcePath: '//fe.benmu-health.com' // 默认你的资源放在 fe.benmu-health.com/vue-tpl/dist/js|css
+};
