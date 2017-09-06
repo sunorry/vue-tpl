b4d4a80e572f051dae2f632b757704e9b15c7c54
sunorry <sunorry@gmail.com>
2017-09-06 10:33:56 +0800

    MOD: add lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,32 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  extends: 'airbnb-base',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // // check if imports actually resolve
-  // 'settings': {
-  //   'import/resolver': {
-  //     'webpack': {
-  //       'config': 'build/webpack.base.conf.js'
-  //     }
-  //   }
-  // },
-  // add your custom rules here
-  'rules': {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
-}
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+      "eslint:recommended",
+      'plugin:vue/recommended'
+    ],
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "vue/valid-v-if": "error",
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
